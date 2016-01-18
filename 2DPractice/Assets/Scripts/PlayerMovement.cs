1dcfe2b066c0b1be670a7bce40e6994a8c47af0a
jtabG <jtabGazzola@gmail.com>
2016-01-18 12:06:14 -0500

    Added comments to PlayerMovement.cs

--- 2DPractice/Assets/Scripts/PlayerMovement.cs
+++ 2DPractice/Assets/Scripts/PlayerMovement.cs
@@ -4,22 +4,43 @@ using System.Collections;
 [RequireComponent(typeof(Rigidbody2D))]
 public class PlayerMovement : MonoBehaviour
 {
+    /// [SerializeField] allows you to view and alter the variable within the Unity editor, while still
+    /// keeping the variable private an inacceable through code outside this class' scope
+    
+    /// <summary>
+    /// Value which modifies the input to determine how quickly to move the character
+    /// </summary>
     [SerializeField]
     private float m_MoveSpeed = 1.0f;
 
+    /// <summary>
+    /// Value which modifies how much force goes into a player jump
+    /// </summary>
     [SerializeField]
     private float m_Jumpforce = 1.0f;
 
+    /// <summary>
+    /// Whether to have the player ignore gravity or not NOT CURRENT IN USE
+    /// </summary>
     [SerializeField]
     private bool m_AffectedByGravity = true;
 
+    /// <summary>
+    /// Reference to player's rigidbody component.
+    /// </summary>
     private Rigidbody2D m_RigidBody;
 
     #region CONST VALUES
+    /// <summary>
+    /// string literals used to avoid typos when looking at input axis
+    /// </summary>
     const string MoveHorStr = "Horizontal";
     const string MoveVertStr = "Vertical";
     const string JumpStr = "Jump";
 
+    /// <summary>
+    /// Small input buffer used to ignore very small input values
+    /// </summary>
     const float IgnoreInputValue = 0.1f;
     #endregion
 
@@ -39,6 +60,9 @@ public class PlayerMovement : MonoBehaviour
         updateJump();
 	}
 
+    /// <summary>
+    /// Resolve input from the user to move the player
+    /// </summary>
     private void updateMovement()
     {
         if (Input.GetAxis(MoveHorStr) > IgnoreInputValue || Input.GetAxis(MoveHorStr) < -IgnoreInputValue)
@@ -49,6 +73,9 @@ public class PlayerMovement : MonoBehaviour
 
     }
 
+    /// <summary>
+    /// Resolve input from the user to cause the player to jump
+    /// </summary>
     private void updateJump()
     {
         if (m_RigidBody.velocity.y < IgnoreInputValue && m_RigidBody.velocity.y > -IgnoreInputValue)
